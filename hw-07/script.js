9ff7a6718c954d3ec5fbfd7c0c03416a8ec91256
Irina Hordiienko <rin.gordienko@gmail.com>
2022-09-25 00:23:37 +0300

    added 2nd task/ 1st option

--- hw-07/script.js
+++ hw-07/script.js
@@ -3,13 +3,29 @@
 // Для решения задачи необходим использовать рекурсию.
 
 function sumTo(x) {
-    if (x === 1) {
-        return 1
-    }
- return x + sumTo(x - 1)
+  if (x === 1) {
+    return 1;
+  }
+  return x + sumTo(x - 1);
 }
 
 console.log(sumTo(4)); // 10 1+2+3+4
 console.log(sumTo(3)); // 6
 
+// 2. Написать функцию findShort , которая будет находит самое короткое слово в предложении.
+// В случае если длина двух слов совпадает - вернуть первое найденое короткое слово.
 
+function findShort(string) {
+  // return array = string
+  //   .split(" ")
+  //   .reduce((a, b) => (a.length < b.length ? a : b));
+  let array = string.split(" ").sort((a, b) => a.length - b.length);
+  return array[0];
+}
+
+let sentence = "Lorem ipsum dolor sit amet";
+
+console.log(findShort(sentence)); // sit
+console.log(findShort("Hello world")); // Hello
+console.log(findShort("Hi")); // Hi
+console.log(findShort("She is David's sister")); // is
