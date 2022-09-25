3abbbd37a7938f6696fdfaf47e18005c9e83f3cc
Irina Hordiienko <rin.gordienko@gmail.com>
2022-09-25 23:20:36 +0300

    added 2nd task - calc

--- hw-08/script.js
+++ hw-08/script.js
@@ -25,3 +25,21 @@ function sumTwoSmallestNumbers(...numbers) {
 console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77)); // 7
 console.log(sumTwoSmallestNumbers(1, 3, 2)); // 3
 console.log(sumTwoSmallestNumbers(1)); // undefined и отдельный console.error("...")
+
+// 2. Написать функцию createCalculator
+
+function createCalculator(firstGivenValue) {
+  return {
+    sum: (secondGivenValue) => (firstGivenValue += secondGivenValue),
+    sub: (secondGivenValue) => (firstGivenValue -= secondGivenValue),
+    mult: (secondGivenValue) => (firstGivenValue *= secondGivenValue),
+    div: (secondGivenValue) => (firstGivenValue /= secondGivenValue),
+  };
+}
+
+const calc = createCalculator(10);
+
+console.log(calc.sum(5)); /// 15
+console.log(calc.mult(10)); // 150
+console.log(calc.sub(40)); // 110
+console.log(calc.div(10)); // 11
