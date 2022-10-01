43e182f1378df9b62d74b2dcbfcd13c0f033c909
Irina Hordiienko <rin.gordienko@gmail.com>
2022-10-01 12:34:16 +0300

    added condition and fixed mistakes

--- hw-11/script.js
+++ hw-11/script.js
@@ -21,12 +21,26 @@ function isWeekend(date) {
   return userDay === 0 || userDay === 6;
 }
 
+function getBirthday(date) {
+  let birthday = new Date(date);
+  let birthdayDay = birthday.getDate();
+  let birthdayMonth = birthday.getMonth() + 1;
+  let birthdayYear = birthday.getFullYear();
+  if (birthdayDay <= 9) {
+    birthdayDay = `0${birthdayDay}`;
+  }
+  if (birthdayMonth <= 9) {
+    birthdayMonth = `0${birthdayMonth}`;
+  }
+  return `${birthdayYear}-${birthdayMonth}-${birthdayDay}`;
+}
+
 class Person {
   constructor(firstName, lastName, age, birthdayDate) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
-    this.birthdayDate = new Date(birthdayDate);
+    this.birthdayDate = getBirthday(birthdayDate);
   }
   celebrate() {
     console.log("Happy Birthday, let's celebrate");
@@ -40,12 +54,17 @@ class Employee extends Person {
     this._salary = salary;
     this.jobPosition = jobPosition;
   }
+
   getYearSalary() {
     return this._salary * 12;
-  }
+  } // не возвращает результат
+
   celebrate() {
-    let currentYear = new Date(this.birthdayDate).getFullYear();
-    let currentBirthday = this.birthdayDate.setFullYear(currentYear);
+    let currentYear = new Date().getFullYear();
+    let currentBirthday = new Date(this.birthdayDate).setFullYear(currentYear);
+
+    // let currentBirthday = new Date(this.birthdayDate);
+    // currentBirthday.setFullYear(new Date().getFullYear());
 
     if (isWeekend(currentBirthday)) {
       return super.celebrate;
@@ -63,9 +82,9 @@ let employee = new Employee(
   "Kate",
   "Dawson",
   33,
-  "1989-03-03",
-  "Painter",
-  1000
+  "1989-10-14",
+  1000,
+  "Painter"
 );
 console.log(employee);
 console.log(employee.celebrate());
