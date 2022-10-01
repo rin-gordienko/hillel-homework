cfd286cc4b5d1ef04f8dbcc77a08f974a46a8098
rin-gordienko <102608359+rin-gordienko@users.noreply.github.com>
2022-10-01 16:48:25 +0300

    deleted the text of the task

--- hw-12/script.js
+++ hw-12/script.js
@@ -1,14 +1,3 @@
-// Создать новый Promise, который будет длится 5 секунд (используем setTimeout) и
-// выполнять следующие действия:
-
-// при успешном завершение (fulfilled) - должен вывести фразу "done"
-// при ошибке (rejected) - вывести текст ошибки в консоль
-// при любом результате вывести текст - "Promise was finished!"
-
-// Обработку промиса необходима написать 2-мя способами:
-
-// .then / .catch
-// async / await + try...catch
 
 let promise = new Promise(function (resolve, reject) {
   setTimeout(() => resolve("done"), 5000);
