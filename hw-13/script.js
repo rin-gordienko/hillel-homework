d93a40db724dcda894b6967781a0e23cdd5d8d4f
Irina Hordiienko <rin.gordienko@gmail.com>
2022-10-07 21:53:19 +0300

    added then/catch

--- hw-13/script.js
+++ hw-13/script.js
@@ -1,13 +1,22 @@
-"use strict"
+"use strict";
 
-
-  let getCompanyUser = async() => {
-    try {
-        let response = await fetch("https://jsonplaceholder.typicode.com/users");
-        let data = await response.json();
-        let users = data.find((user => user.company.name === "Johns Group"));
-        console.log(users)
-    } catch (error) {
-        console.log(error)
-    }
+let getCompanyUser = async () => {
+  try {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+    let users = data.find((user) => user.company.name === "Johns Group");
+    console.log(users);
+  } catch (error) {
+    console.log(error);
   }
+};
+
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((result) => {
+//     let users = result.find((user) => user.company.name === "Johns Group");
+//     console.log(users);
+//   })
+
+//   .catch((error) => console.log(error)); 
