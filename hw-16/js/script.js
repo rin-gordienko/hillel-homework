4be866ea4f331ba2b85859ee70f0d7525873f740
Irina Hordiienko <rin.gordienko@gmail.com>
2022-10-18 00:17:56 +0300

    added task, styles

--- hw-16/js/script.js
+++ hw-16/js/script.js
@@ -1,30 +1,26 @@
 "use strict";
 
+const albumsFolder = document.getElementById("albums");
+
+albumsFolder.onclick = (event) => {
+  if (event.target.className === "remove-btn") {
+    event.target.closest(".album_title").remove();
+  }
+};
+
 fetch("https://jsonplaceholder.typicode.com/albums")
   .then((response) => response.json())
   .then((albums) => {
     for (let album of albums) {
-      let list = document.createElement("li");
-      list.innerHTML = album.title;
-      list.classList.add("album_title");
-      document.getElementById("albums").append(list);
+      let albumItem = document.createElement("li");
+      albumItem.innerHTML = album.title;
+      albumItem.classList.add("album_title");
+      albumsFolder.append(albumItem);
+
+      let removeBtn = document.createElement("button");
+      removeBtn.innerHTML = "x";
+      removeBtn.classList.add("remove-btn");
+      albumItem.append(removeBtn);
     }
   })
-
   .catch((error) => console.log(error));
-
-// let getAlbums = async () => {
-//   try {
-//     let response = await fetch("https://jsonplaceholder.typicode.com/albums");
-//     let albums = await response.json();
-//     for (let album of albums) {
-//       let list = document.createElement("li");
-//       list.innerHTML = album.title;
-//       list.classList.add("album_title");
-//       document.getElementById("albums").append(list);
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// getAlbums();
